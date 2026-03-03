4ea11018b3c7927defbc7e300a62a34562c8f891
Olly <olly.sawers25@students.bhasvic.ac.uk>
2026-03-03 15:01:27 +0000

    first commit

---
+++ src/Cleaner.java
@@ -0,0 +1,7 @@
+public class Cleaner {
+	private String name;
+	
+    Cleaner(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Receptionist.java
@@ -0,0 +1,7 @@
+public class Receptionist {
+    private String name;
+    
+	Receptionist(String name) {
+        this.name = name;
+	}
+}
